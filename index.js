d2ff16eaaf3e1427f00d8180fb0b1ee5652692a3
sreevaniquin <sreevani@quintessential.design>
2022-07-21 15:20:58 +0530

    index changed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const bodyParser=require('body-parser')
 const mongoose=require('mongoose')
+const port = process.env.PORT || 3000
 const app=express()
 app.use(bodyParser.json())
 app.use(express.static('public'))//static file is available in public directory
@@ -46,5 +47,9 @@ app.post("/sign_up",(req,res)=>{
     status:true,
     msg:"successfull"
    });
-}).listen(3000);
-console.log("listening on port 3000");
+})
+
+
+app.listen(port, ()=>{
+console.log(`Listening at http://localhost:${port}`);
+})
